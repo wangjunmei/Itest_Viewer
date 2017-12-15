be2d97cc870d454c5517856a885cd01d56b17901
vanessa <1796022660@qq.com>
2017-12-15 16:29:28 +0800

    add fadein  animation

--- hoverinspect.js
+++ hoverinspect.js
@@ -383,6 +383,7 @@ for (var x=0;x<elementArry.length;x++)
     },
 
       deactivate: function() {
+         this.$wrap.classList.add('-out');
           document.removeEventListener('contextmenu', this.log);
           setTimeout(function() {
             document.body.removeChild(this.$host);
